ef418e31b3fafb7b0b1e418794131e94e9403c56
Evan Pearson <ejp1989@gmail.com>
2023-03-31 21:13:43 -0700

    add extra icons

--- augur/cli.py
+++ augur/cli.py
@@ -5,13 +5,23 @@ colors = {
     'burning_fireflies' : '#ff166c'
 }
 
+emojis = {
+    '8ball': '🎱',
+    'crystal': '🔮',
+}
 
 augur_theme = Theme({
-    'icon': f'bold {colors["burning_fireflies"]} reverse'
+    'icon': f'bold {colors["burning_fireflies"]} reverse',
+    'header': f'bold {colors["burning_fireflies"]}'
 })
 
 console = Console(theme=augur_theme)
 
-icon = "[icon] augur :[/icon]"
+icons = { 
+    'text': "[icon] augur :[/icon]",
+    '8ball': f"{emojis['8ball']}[header] : [/header]",
+    'crystal': f"{emojis['crystal']}[header] : [/header]",
+}
+
 
 print = console.print
